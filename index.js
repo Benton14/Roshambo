8a98f14974768d9d52e75377ab495255dbf8a308
Benton14 <awoodham14@gmail.com>
2024-01-22 14:36:36 -0600

    Add Computer choice function for random selection

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const getComputerChoice = Math.floor(Math.random() * 3) + 1;
+const randomNumber = Math.floor(Math.random() * 3) + 1;
+let choice;
 
-console.log(getComputerChoice);
+function getComputerChoice() {
+    if (randomNumber === 1 ) {
+        choice = "rock"
+    } else if (randomNumber === 2) {
+        choice = "paper"
+    } else if (randomNumber === 3 ) {
+        choice = "scissors"
+    }
+}
+
+getComputerChoice();
+console.log(choice);
