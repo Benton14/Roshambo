1f0831abf13e8c0b15be1ce2246ad99c72734a2b
Benton14 <awoodham14@gmail.com>
2024-02-02 16:03:05 -0600

    Create buttons for player choice & add style sheet
    added buttons for player choice & style sheet.
    commented out rps game functionality because it needs to be reworked
    with new buttons & comp choice.

--- index.js
+++ index.js
@@ -14,50 +14,72 @@ function getComputerChoice() {
     return choice;
 }
 
+let compPick = getComputerChoice();
+console.log(compPick);
 
-function round() {
-    const compChoice = getComputerChoice();
-    const myChoice = prompt("What's your move?").toLowerCase();
+function playerChoice() {
+    const btnOne = document.getElementById("rock");
+    const btntwo = document.getElementById("paper");
+    const btnThree = document.getElementById("scissors");
 
-    if (compChoice === "rock") {
-        if (myChoice === "rock") {
-            console.log("It's a tie! Play again")
-        } else if (myChoice === "paper") {
-            console.log("You Win!")
-        } else if (myChoice === "scissors") {
-            console.log("You Lose.")
-        } else {
-            console.log("invalid input. Please choose rock, paper, or scissors.")
-        }
-    } else if (compChoice === "paper") {
-        if (myChoice === "rock") {
-            console.log("You Lose.")
-        } else if (myChoice === "paper") {
-            console.log("It's a tie! Play again")
-        } else if (myChoice === "scissors") {
-            console.log("You Win!")
-        } else {
-            console.log("invalid input. Please choose rock, paper, or scissors.")
-        }
-    } else if (compChoice === "scissors") {
-        if (myChoice === "rock") {
-            console.log("You Win!")
-        } else if (myChoice === "paper") {
-            console.log("You Lose.")
-        } else if (myChoice === "scissors") {
-            console.log("It's a tie! Play again")
-        } else {
-            console.log("invalid input. Please choose rock, paper, or scissors.")
-        }
-    }  
-    console.log("Computer: " + compChoice);
-    console.log("Player: " + myChoice);
+    btnOne.addEventListener("click", function(e) {
+        console.log("rock");
+    });
+
+    btntwo.addEventListener("click", function(e) {
+        console.log("paper");
+    });
+
+    btnThree.addEventListener("click", function(e) {
+        console.log("scissors");
+    });
 }
 
-function game() {
-    for (i=0;i<=5;i++) {
-        round();
-    }
-}
+playerChoice();
 
-game();
+// function round() {
+//     const compChoice = getComputerChoice();
+//     const myChoice = playerChoice();
+
+//     if (compChoice === "rock") {
+//         if (myChoice === "rock") {
+//             console.log("It's a tie! Play again")
+//         } else if (myChoice === "paper") {
+//             console.log("You Win!")
+//         } else if (myChoice === "scissors") {
+//             console.log("You Lose.")
+//         } else {
+//             console.log("invalid input. Please choose rock, paper, or scissors.")
+//         }
+//     } else if (compChoice === "paper") {
+//         if (myChoice === "rock") {
+//             console.log("You Lose.")
+//         } else if (myChoice === "paper") {
+//             console.log("It's a tie! Play again")
+//         } else if (myChoice === "scissors") {
+//             console.log("You Win!")
+//         } else {
+//             console.log("invalid input. Please choose rock, paper, or scissors.")
+//         }
+//     } else if (compChoice === "scissors") {
+//         if (myChoice === "rock") {
+//             console.log("You Win!")
+//         } else if (myChoice === "paper") {
+//             console.log("You Lose.")
+//         } else if (myChoice === "scissors") {
+//             console.log("It's a tie! Play again")
+//         } else {
+//             console.log("invalid input. Please choose rock, paper, or scissors.")
+//         }
+//     }  
+//     console.log("Computer: " + compChoice);
+//     console.log("Player: " + myChoice);
+// }
+
+// function game() {
+//     for (i=0;i<=5;i++) {
+//         round();
+//     }
+// }
+
+// game();
